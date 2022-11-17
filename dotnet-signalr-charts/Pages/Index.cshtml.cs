30c3e79c4f38867813bd837894434f7ff0e4be24
KTECH\Cihan <c.tekin@keesingtechnologies.com>
2022-11-17 23:00:06 +0100

    Add project files.

---
+++ dotnet-signalr-charts/Pages/Index.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace dotnet_signalr_charts.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
