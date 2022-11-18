99c2fe11a0d4eee0c3bd6f010e9cdbe2f9483a91
KTECH\Cihan <c.tekin@keesingtechnologies.com>
2022-11-18 22:57:01 +0100

    libman.json included and Buffer service created

---
+++ dotnet-signalr-charts/Services/Buffer.cs
@@ -0,0 +1,19 @@
+﻿namespace dotnet_signalr_charts.Services
+{
+    public class Buffer<T> : Queue<T>
+    {
+        public int? MaxCapacity { get; set; }
+        public Buffer(int capacity) { MaxCapacity = capacity; }
+        public int TotalItemsAddedCount { get; private set; }
+
+        public void Add(T item) {
+            if (Count == (MaxCapacity ?? -1))
+            {
+                Dequeue();
+            }
+
+            Enqueue(item);
+            TotalItemsAddedCount++;
+        } 
+    }
+}
