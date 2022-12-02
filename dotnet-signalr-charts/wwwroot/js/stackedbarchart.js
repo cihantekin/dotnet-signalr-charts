97c1495985293350e3e9045b08128479e093e5e4
KTECH\Cihan <c.tekin@keesingtechnologies.com>
2022-12-03 00:40:12 +0100

    StackBarChart html and javascript work

---
+++ dotnet-signalr-charts/wwwroot/js/stackedbarchart.js
@@ -0,0 +1,24 @@
+﻿const data = JSON.parse(document.getElementById('data').innerHTML);
+const ctx = document.getElementById('myChart').getContext('2d');
+
+const myChart = new Chart(ctx, {
+    type: 'bar',
+    data: data,
+    options: {
+        plugins: {
+            title: {
+                display: true,
+                text: 'Chart.js Bar Chart - Stacked'
+            },
+        },
+        responsive: true,
+        scales: {
+            x: {
+                stacked: true,
+            },
+            y: {
+                stacked: true
+            }
+        }
+    }
+});
