1184eaba34634b4f75999b2aca47538ce474c386
KTECH\Cihan <c.tekin@keesingtechnologies.com>
2022-12-02 12:36:46 +0100

    StackedBarChart.cshtml created

---
+++ dotnet-signalr-charts/Pages/StackedBarChart.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace dotnet_signalr_charts.Pages
+{
+    public class StackedBarChartModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
